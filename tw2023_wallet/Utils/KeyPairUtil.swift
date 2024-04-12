ab74c5645e1815af028889b22d37aa6e710cd4bd
katsuyoshi ozaki <ozaki@datasign.jp>
2024-04-12 12:33:35 +0900

    Fix decoding errors

--- tw2023_wallet/Utils/KeyPairUtil.swift
+++ tw2023_wallet/Utils/KeyPairUtil.swift
@@ -140,10 +140,10 @@ class KeyPairUtil {
     
     static func createPublicKey(jwk: [String: String]) throws -> SecKey {
         // Check if the necessary JWK components are present
-        guard let xBase64 = jwk["x"],
-              let yBase64 = jwk["y"],
-              let xData = Data(base64Encoded: xBase64),
-              let yData = Data(base64Encoded: yBase64) else {
+        guard let xBase64Url = jwk["x"],
+              let yBase64Url = jwk["y"],
+              let xData = xBase64Url.base64UrlDecoded(),
+              let yData = yBase64Url.base64UrlDecoded() else {
             throw JwkError.UnableToConversionError
         }
         
