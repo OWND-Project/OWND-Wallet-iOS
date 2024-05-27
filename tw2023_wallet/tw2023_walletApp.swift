b5d5fae0b4a87df1bbacb6405b69323a32f6c733
katsuyoshi ozaki <ozaki@datasign.jp>
2024-05-27 17:47:23 +0900

    Add Universal Link and Custom Schemes

--- tw2023_wallet/tw2023_walletApp.swift
+++ tw2023_wallet/tw2023_walletApp.swift
@@ -9,30 +9,39 @@ import SwiftUI
 import CoreData
 import LocalAuthentication
 
+
 func createCredentialOfferArgs(value: String) -> CredentialOfferArgs{
     let args = CredentialOfferArgs()
     args.credentialOffer = value
     return args
 }
 
+
+func createOpenID4VPArgs(value: String) -> SharingCredentialArgs {
+    let args = SharingCredentialArgs()
+    args.url = value
+    return args
+}
+
+
 @main
 struct tw2023_walletApp: App {
     @State private var isShowingCredentialOffer = false
     @State private var credentialOffer: String? = nil
+    @State private var isShowingOpenID4VP = false
+    @State private var openID4VP: String? = nil
+    
+    @State private var sharingRequestModel = SharingRequestModel()
+    
     private var authenticationManager = AuthenticationManager()
     @Environment(\.scenePhase) private var scenePhase
-    
+
     var body: some Scene {
         WindowGroup {
             if self.authenticationManager.isUnlocked {
                 ContentView()
                     .onOpenURL(perform: { url in
-                        if url.scheme == "openid-credential-offer" {
-                            print("Credential Offerを受け取りました")
-                            print(url.absoluteString)
-                            self.credentialOffer = url.absoluteString
-                            self.isShowingCredentialOffer = true
-                        }
+                        handleIncomingURL(url)
                     })
                     .fullScreenCover(isPresented: $isShowingCredentialOffer, onDismiss: {
                         credentialOffer = nil
@@ -43,31 +52,76 @@ struct tw2023_walletApp: App {
                             EmptyView()
                         }
                     }
+                    .fullScreenCover(isPresented: $isShowingOpenID4VP, onDismiss: {
+                        openID4VP = nil
+                    }) {
+                        if let value = openID4VP {
+                            SharingRequest(args: createOpenID4VPArgs(value: value)).environment(sharingRequestModel)
+                        } else {
+                            EmptyView()
+                        }
+                    }
             } else {
                 AuthenticationView(authenticationManager: self.authenticationManager)
             }
         }
         .environment(authenticationManager)
-        .onChange(of: scenePhase) {
-            switch scenePhase {
-            case .active:
-                print("App is active")
-            case .inactive:
-                print("App is inactive")
-            case .background:
-                print("App is in background")
-                if self.authenticationManager.shouldLock() {
-                    self.authenticationManager.isUnlocked = false
-                    // ロック解除後に CredentialOfferViewが意図せず表示されてしまう問題の修正
-                    if (credentialOffer != nil) {
-                        credentialOffer = nil
-                    }
+        .onChange(of: scenePhase) { 
+            handleScenePhaseChange(scenePhase)
+        }
+    }
+    
+    private func handleScenePhaseChange(_ newPhase: ScenePhase) {
+        switch newPhase {
+        case .active:
+            print("App is active")
+        case .inactive:
+            print("App is inactive")
+        case .background:
+            print("App is in background")
+            if self.authenticationManager.shouldLock() {
+                self.authenticationManager.isUnlocked = false
+                if credentialOffer != nil {
+                    credentialOffer = nil
                 }
-            @unknown default:
-                break
+            }
+        @unknown default:
+            break
+        }
+    }
+    
+    private func handleIncomingURL(_ url: URL) {
+        print("handling url : \(url)")
+        if url.scheme == "openid4vp" {
+            handleVp(url)
+        } else if url.scheme == "openid-credential-offer" {
+            handleOffer(url)
+        } else if let components = URLComponents(url: url, resolvingAgainstBaseURL: false) {
+            if let queryItems = components.queryItems {
+                let credentialOfferParam = queryItems.first { $0.name == "credential_offer" }?.value
+                if credentialOfferParam != nil {
+                    handleOffer(url)
+                } else {
+                    handleVp(url)
+                }
+            } else {
+                print("query not found")
             }
         }
     }
+
+    private func handleOffer(_ url: URL) {
+        print("credential offer")
+        credentialOffer = url.absoluteString
+        isShowingCredentialOffer = true
+    }
+
+    private func handleVp(_ url: URL) {
+        print("vp")
+        openID4VP = url.absoluteString
+        isShowingOpenID4VP = true
+    }
+    
 }
 
 enum ApplicatoinError: Error {
