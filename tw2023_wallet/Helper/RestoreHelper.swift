e9ba4b4c71cae97bad37252bf64183a7f842f7a9
katsuyoshi ozaki <ozaki@datasign.jp>
2024-06-07 13:45:15 +0900

    Add `RestoreHelper` and refactor test files.

---
+++ tw2023_wallet/Helper/RestoreHelper.swift
@@ -0,0 +1,8 @@
+//
+//  RestoreHelper.swift
+//  tw2023_wallet
+//
+//  Created by katsuyoshi ozaki on 2024/06/07.
+//
+
+import Foundation
---
+++ tw2023_walletTests/Helper/RestoreHelperTests.swift
@@ -0,0 +1,8 @@
+//
+//  RestoreHelperTests.swift
+//  tw2023_walletTests
+//
+//  Created by katsuyoshi ozaki on 2024/06/07.
+//
+
+import Foundation
