34a34e2cc2dd795163298dae69f4c634367498c2
katsuyoshi ozaki <ozaki@datasign.jp>
2024-06-24 18:23:30 +0900

    Separate test cases

--- tw2023_walletTests/VCIMetadataTests.swift
+++ tw2023_walletTests/VCIMetadataTests.swift
@@ -7,8 +7,7 @@
 
 import XCTest
 
-final class VCIMetadataTests: XCTestCase {
-
+final class CredentialOfferTests: XCTestCase {
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -16,16 +15,58 @@ final class VCIMetadataTests: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
+    
+    func testDeserializeFilledCredentialOffer() throws {
+        let jsonData = try loadJsonTestData(fileName: "credential_offer_filled")
+        let decoder = JSONDecoder()
+        let credentialOffer = try decoder.decode(CredentialOffer.self, from: jsonData)
 
+        XCTAssertEqual(credentialOffer.credentialIssuer, "https://datasign-demo-vci.tunnelto.dev")
+        XCTAssertFalse(credentialOffer.credentialConfigurationIds.isEmpty)
+        XCTAssertEqual(credentialOffer.credentialConfigurationIds[0], "IdentityCredential")
+
+        let grants = credentialOffer.grants
+        XCTAssertEqual(grants?.authorizationCode?.issuerState, "eyJhbGciOiJSU0Et...FYUaBy")
+        
+        XCTAssertEqual(grants?.preAuthorizedCode?.preAuthorizedCode, "adhjhdjajkdkhjhdj")
+        XCTAssertEqual(grants?.preAuthorizedCode?.txCode?.inputMode, "numeric")
+        XCTAssertEqual(grants?.preAuthorizedCode?.txCode?.length, 4)
+        XCTAssertEqual(grants?.preAuthorizedCode?.txCode?.description, "description")
+        XCTAssertEqual(grants?.preAuthorizedCode?.interval, 10)
+        XCTAssertEqual(grants?.preAuthorizedCode?.authorizationServer, "https://datasign-demo-vci.tunnelto.dev")
+    }
+    
+    func testDeserializeMinimumCredentialOffer() throws {
+        let jsonData = try loadJsonTestData(fileName: "credential_offer_minimum")
+        let decoder = JSONDecoder()
+        let credentialOffer = try decoder.decode(CredentialOffer.self, from: jsonData)
+
+        XCTAssertEqual(credentialOffer.credentialIssuer, "https://datasign-demo-vci.tunnelto.dev")
+        XCTAssertFalse(credentialOffer.credentialConfigurationIds.isEmpty)
+        XCTAssertEqual(credentialOffer.credentialConfigurationIds[0], "IdentityCredential")
+
+        XCTAssertNil(credentialOffer.grants)
+    }
+    func testDeserializeCredentialOfferWithTxCode() throws {
+        let jsonData = try loadJsonTestData(fileName: "credential_offer_tx_code_required")
+        let decoder = JSONDecoder()
+        let credentialOffer = try decoder.decode(CredentialOffer.self, from: jsonData)
+        
+        XCTAssertTrue(credentialOffer.isTxCodeRequired())
+    }
+}
+
+final class CredentialDisplayTests: XCTestCase {
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
     func testDeserializeFilledCredentialDisplay() throws {
-        guard
-            let url = Bundle.main.url(forResource: "filled_all_parameters", withExtension: "json"),
-            let jsonData = try? Data(contentsOf: url)
-        else {
-            XCTFail("Cannot read test data")
-            return
-        }
-
+        let jsonData = try loadJsonTestData(fileName: "credential_display_filled")
         let decoder = JSONDecoder()
         let display = try decoder.decode(CredentialDisplay.self, from: jsonData)
 
@@ -40,13 +81,7 @@ final class VCIMetadataTests: XCTestCase {
     }
 
     func testDeserializeMinimumCredentialDisplay() throws {
-        guard let url = Bundle.main.url(forResource: "minimum", withExtension: "json"),
-            let jsonData = try? Data(contentsOf: url)
-        else {
-            XCTFail("Cannot read test data")
-            return
-        }
-
+        let jsonData = try loadJsonTestData(fileName: "credential_display_minimum")
         let decoder = JSONDecoder()
         let display = try decoder.decode(CredentialDisplay.self, from: jsonData)
 
@@ -58,15 +93,154 @@ final class VCIMetadataTests: XCTestCase {
         XCTAssertNil(display.backgroundImage)
         XCTAssertNil(display.textColor)
     }
+}
 
-    func testDeserializeJsonCredentialSubject() throws {
-        guard let url = Bundle.main.url(forResource: "partial_optional", withExtension: "json"),
-            let jsonData = try? Data(contentsOf: url)
+
+final class CredentialSupportedTests: XCTestCase {
+     override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+   
+    func testDecodeCredentialSupportedJwtVcJson() throws {
+        let jsonData = try loadJsonTestData(fileName: "credential_supported_jwt_vc")
+
+        let credentialSupported = try decodeCredentialSupported(from: jsonData)
+        if let credential = credentialSupported as? CredentialSupportedJwtVcJson {
+            XCTAssertEqual(credential.scope, "UniversityDegree")
+            if let credentialSubject = credential.credentialDefinition.credentialSubject {
+                if let givenNameSubject = credentialSubject["given_name"] {
+                    if let display = givenNameSubject.display, display.count > 0 {
+                        XCTAssertEqual(display[0].name, "Given Name")
+                        XCTAssertEqual(display[0].locale, "en-US")
+                    }
+                    else {
+                        XCTFail("Display data for 'given_name' is missing or empty")
+                    }
+                }
+                else {
+                    XCTFail("'given_name' key is missing in the decoded map")
+                }
+            }
+            else {
+                XCTFail("credentialSubjectis missing in the decoded map")
+            }
+        }
         else {
-            XCTFail("Cannot read test data")
-            return
+            XCTFail("Decoded type is not CredentialSupportedJwtVcJson")
+        }
+    }
+
+    func testDecodeCredentialSupportedVcSdJwt() throws {
+        let jsonData = try loadJsonTestData(fileName: "credential_supported_vc_sd_jwt")
+        let credentialSupported = try decodeCredentialSupported(from: jsonData)
+        if let credential = credentialSupported as? CredentialSupportedVcSdJwt {
+            XCTAssertEqual(credential.scope, "EmployeeIdentification")
+            if let credentialSubject = credential.claims {
+                if let givenNameSubject = credentialSubject["given_name"] {
+                    if let display = givenNameSubject.display, display.count > 0 {
+                        XCTAssertEqual(display[0].name, "Given Name")
+                        XCTAssertEqual(display[0].locale, "en-US")
+                    }
+                    else {
+                        XCTFail("Display data for 'given_name' is missing or empty")
+                    }
+                }
+                else {
+                    XCTFail("'given_name' key is missing in the decoded map")
+                }
+            }
+            else {
+                XCTFail("claims missing in the decoded map")
+            }
+        }
+        else {
+            XCTFail("Decoded type is not CredentialSupportedVcSdJwt")
+        }
+    }
+
+    func testDecodeCredentialSupportedLdpVc() throws {
+        let jsonData = try loadJsonTestData(fileName: "credential_supported_ldp_vc")
+        let credentialSupported = try decodeCredentialSupported(from: jsonData)
+        if let credential = credentialSupported as? CredentialSupportedLdpVc {
+            XCTAssertEqual(credential.credentialDefinition.type[1], "UniversityDegreeCredential")
+            if let credentialSubject = credential.credentialDefinition.credentialSubject {
+                if let givenNameSubject = credentialSubject["given_name"] {
+                    if let display = givenNameSubject.display, display.count > 0 {
+                        XCTAssertEqual(display[0].name, "Given Name")
+                        XCTAssertEqual(display[0].locale, "en-US")
+                    }
+                    else {
+                        XCTFail("Display data for 'given_name' is missing or empty")
+                    }
+                }
+                else {
+                    XCTFail("'given_name' key is missing in the decoded map")
+                }
+            }
+            else {
+                XCTFail("claims missing in the decoded map")
+            }
+        }
+        else {
+            XCTFail("Decoded type is not CredentialSupportedVcSdJwt")
+        }
+    }
+}
+
+
+final class ClaimMapTests: XCTestCase {
+    
+    func testDeserializeEmptyClaimMap() throws {
+        let jsonData = try loadJsonTestData(fileName: "claim_map_empty")
+        let decoder = JSONDecoder()
+        let claimMap = try decoder.decode(ClaimMap.self, from: jsonData)
+        
+        XCTAssertTrue(claimMap.isEmpty)
+    }
+    
+    func testDeserializeFilledClaimMap() throws {
+        let jsonData = try loadJsonTestData(fileName: "claim_map_filled")
+        let decoder = JSONDecoder()
+        let claimMap = try decoder.decode(ClaimMap.self, from: jsonData)
+         // 各プロパティに対するテストケース
+        if let givenNameClaim = claimMap["given_name"] {
+            XCTAssertEqual(givenNameClaim.mandatory, true)
+            XCTAssertEqual(givenNameClaim.valueType, "string")
+            XCTAssertEqual(givenNameClaim.display?.count, 2)
+            
+            XCTAssertEqual(givenNameClaim.display?.first?.name, "Given Name")
+            XCTAssertEqual(givenNameClaim.display?.first?.locale, "en-US")
+            
+            XCTAssertEqual(givenNameClaim.display?[1].name, "名")
+            XCTAssertEqual(givenNameClaim.display?[1].locale, "ja-JP")
+           
+        }
+        else {
+            XCTFail("given_name claim is missing")
         }
 
+        if let lastNameClaim = claimMap["last_name"] {
+            XCTAssertEqual(lastNameClaim.mandatory, false)
+            XCTAssertEqual(lastNameClaim.valueType, "number")
+            XCTAssertEqual(lastNameClaim.display?.count, 2)
+            XCTAssertEqual(lastNameClaim.display?.first?.name, "Surname")
+            XCTAssertEqual(lastNameClaim.display?.first?.locale, "en-US")
+
+            XCTAssertEqual(lastNameClaim.display?[1].name, "姓")
+            XCTAssertEqual(lastNameClaim.display?[1].locale, "ja-JP")
+        }
+        else {
+            XCTFail("last_name claim is missing")
+        }
+       
+    }
+    
+    func testDeserializeMixMandatoryAndNonMandatoryClaimMap() throws {
+        let jsonData = try loadJsonTestData(fileName: "claim_map_mixed")
         let decoder = JSONDecoder()
         let claimMap = try decoder.decode(ClaimMap.self, from: jsonData)
 
@@ -137,116 +311,22 @@ final class VCIMetadataTests: XCTestCase {
 
     }
 
-    func testDecodeCredentialSupportedJwtVcJson() throws {
-        // テスト用のJSONファイルを読み込む
-        guard
-            let url = Bundle.main.url(
-                forResource: "credential_supported_jwt_vc",
-                withExtension: "json"),
-            let jsonData = try? Data(contentsOf: url)
-        else {
-            XCTFail("Cannot read test data")
-            return
-        }
+}
 
-        let credentialSupported = try decodeCredentialSupported(from: jsonData)
-        if let credential = credentialSupported as? CredentialSupportedJwtVcJson {
-            XCTAssertEqual(credential.scope, "UniversityDegree")
-            if let credentialSubject = credential.credentialDefinition.credentialSubject {
-                if let givenNameSubject = credentialSubject["given_name"] {
-                    if let display = givenNameSubject.display, display.count > 0 {
-                        XCTAssertEqual(display[0].name, "Given Name")
-                        XCTAssertEqual(display[0].locale, "en-US")
-                    }
-                    else {
-                        XCTFail("Display data for 'given_name' is missing or empty")
-                    }
-                }
-                else {
-                    XCTFail("'given_name' key is missing in the decoded map")
-                }
-            }
-            else {
-                XCTFail("credentialSubjectis missing in the decoded map")
-            }
-        }
-        else {
-            XCTFail("Decoded type is not CredentialSupportedJwtVcJson")
-        }
+
+
+
+
+
+
+final class VCIMetadataTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
-    func testDecodeCredentialSupportedVcSdJwt() throws {
-        // テスト用のJSONファイルを読み込む
-        guard
-            let url = Bundle.main.url(
-                forResource: "credential_supported_vc_sd_jwt", withExtension: "json"),
-            let jsonData = try? Data(contentsOf: url)
-        else {
-            XCTFail("Cannot read test data")
-            return
-        }
-
-        let credentialSupported = try decodeCredentialSupported(from: jsonData)
-        if let credential = credentialSupported as? CredentialSupportedVcSdJwt {
-            XCTAssertEqual(credential.scope, "EmployeeIdentification")
-            if let credentialSubject = credential.claims {
-                if let givenNameSubject = credentialSubject["given_name"] {
-                    if let display = givenNameSubject.display, display.count > 0 {
-                        XCTAssertEqual(display[0].name, "Given Name")
-                        XCTAssertEqual(display[0].locale, "en-US")
-                    }
-                    else {
-                        XCTFail("Display data for 'given_name' is missing or empty")
-                    }
-                }
-                else {
-                    XCTFail("'given_name' key is missing in the decoded map")
-                }
-            }
-            else {
-                XCTFail("claims missing in the decoded map")
-            }
-        }
-        else {
-            XCTFail("Decoded type is not CredentialSupportedVcSdJwt")
-        }
-    }
-
-    func testDecodeCredentialSupportedLdpVc() throws {
-        // テスト用のJSONファイルを読み込む
-        guard
-            let url = Bundle.main.url(
-                forResource: "credential_supported_ldp_vc", withExtension: "json"),
-            let jsonData = try? Data(contentsOf: url)
-        else {
-            XCTFail("Cannot read credential_supported_ldp_vc.json")
-            return
-        }
-
-        let credentialSupported = try decodeCredentialSupported(from: jsonData)
-        if let credential = credentialSupported as? CredentialSupportedLdpVc {
-            XCTAssertEqual(credential.credentialDefinition.type[1], "UniversityDegreeCredential")
-            if let credentialSubject = credential.credentialDefinition.credentialSubject {
-                if let givenNameSubject = credentialSubject["given_name"] {
-                    if let display = givenNameSubject.display, display.count > 0 {
-                        XCTAssertEqual(display[0].name, "Given Name")
-                        XCTAssertEqual(display[0].locale, "en-US")
-                    }
-                    else {
-                        XCTFail("Display data for 'given_name' is missing or empty")
-                    }
-                }
-                else {
-                    XCTFail("'given_name' key is missing in the decoded map")
-                }
-            }
-            else {
-                XCTFail("claims missing in the decoded map")
-            }
-        }
-        else {
-            XCTFail("Decoded type is not CredentialSupportedVcSdJwt")
-        }
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
     func testDecodeCredentialIssuerMetadata() throws {
@@ -266,34 +346,4 @@ final class VCIMetadataTests: XCTestCase {
         // 他のプロパティに関するアサーションも同様に行います。
     }
 
-    // todo: move to another file.
-    func testDeserializeJsonCredentialOffer() throws {
-        guard let url = Bundle.main.url(forResource: "credential_offer", withExtension: "json"),
-            let jsonData = try? Data(contentsOf: url)
-        else {
-            XCTFail("Cannot read test data")
-            return
-        }
-
-        let decoder = JSONDecoder()
-        let credentialOffer = try decoder.decode(CredentialOffer.self, from: jsonData)
-
-        XCTAssertEqual(credentialOffer.credentialIssuer, "https://datasign-demo-vci.tunnelto.dev")
-        XCTAssertFalse(credentialOffer.credentialConfigurationIds.isEmpty)
-        XCTAssertEqual(credentialOffer.credentialConfigurationIds[0], "IdentityCredential")
-
-        let grants = credentialOffer.grants
-        XCTAssertEqual(grants?.authorizationCode?.issuerState, "eyJhbGciOiJSU0Et...FYUaBy")
-        XCTAssertEqual(grants?.preAuthorizedCode?.preAuthorizedCode, "adhjhdjajkdkhjhdj")
-        XCTAssertTrue(credentialOffer.isTxCodeRequired())
-
-        XCTAssertEqual(grants?.preAuthorizedCode?.txCode?.inputMode, "numeric")
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
 }
